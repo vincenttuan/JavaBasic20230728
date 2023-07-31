8c40a25c896f757e6ffa7bc9975da1f134ef4e52
VincentTuan <vincentjava@yahoo.com.tw>
2023-07-31 19:42:33 +0800

    Add files via upload

---
+++ javacode/day2/Test1.java
@@ -0,0 +1,9 @@
+package day2;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println(98_7654_3212L);
+	}
+
+}
---
+++ javacode/day2/Test2.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		int a = 0b011;
+		int b = 017;
+		int c = 0xFF;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
