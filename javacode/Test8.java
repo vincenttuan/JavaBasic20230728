746961f38700fd1a22e00f5de55febba969bb45e
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-04 21:30:23 +0800

    Add files via upload

---
+++ javacode/Test8.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		int score = 75;
+		// 等級區分
+		// 90分以上為"A", 80分以上為"B", 70分以上為"C", 60分以上為"D", 59分以下為"F"
+		// 請問 score 分數的等級
+		// 限用 ?:
+		
+
+	}
+
+}
