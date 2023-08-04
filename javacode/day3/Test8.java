0e459b2f4b7496f6f1a6fc317492490649efd994
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-04 21:34:13 +0800

    Add files via upload

--- javacode/day3/Test8.java
+++ javacode/day3/Test8.java
@@ -8,7 +8,9 @@ public class Test8 {
 		// 90分以上為"A", 80分以上為"B", 70分以上為"C", 60分以上為"D", 59分以下為"F"
 		// 請問 score 分數的等級
 		// 限用 ?:
-		
+		String grade = (score >= 90) ? "A" : (score >= 80) ? "B" : (score >= 70) ? "C" : (score >= 60) ? "D" : "F";
+		System.out.println(score);
+		System.out.println(grade);
 
 	}
 
