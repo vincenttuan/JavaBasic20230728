ced125cc29e1d00e76bf8d657ae19a3273610d89
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-04 19:53:00 +0800

    Add files via upload

--- javacode/day3/Test4.java
+++ javacode/day3/Test4.java
@@ -3,20 +3,21 @@ package day3;
 題目:
 小七飲料特價
 飲料一瓶 20 元
-買 3 送 1
+買 n 送 1
 請問買 22 瓶需要多少元 ?
 */
 public class Test4 {
 	public static void main(String[] args) {
 		int bottlesToBuy = 22;  // 要買 22 瓶
 		int pricePerBottle = 20; // 飲料一瓶 20 元
+		int n = 1;
 		
 		// 1. 分組
-		int groups = bottlesToBuy / 4;
+		int groups = bottlesToBuy / (n+1);
 		// 2. 每一組要付的價格之總和
-		int groupsToPayFor = groups * (pricePerBottle * 3); 
+		int groupsToPayFor = groups * (pricePerBottle * n); 
 		// 3. 檢查是否有餘數 ? 再乘上每一瓶的價格
-		int remainingToPayFor = (bottlesToBuy % 4) * pricePerBottle;
+		int remainingToPayFor = (bottlesToBuy % (n+1)) * pricePerBottle;
 
 		int totalPrice = groupsToPayFor + remainingToPayFor;
 
