43e14ee78381238a93ed00b0f9c71ea29f823869
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-07 19:46:07 +0800

    Add files via upload

--- javacode/day4/Test3.java
+++ javacode/day4/Test3.java
@@ -9,8 +9,30 @@ public class Test3 {
 		// 70~79  -> C, Well done
 		// 60~69  -> D, You passed
 		// 0~59   -> F, Better try again
-		// use java switch-case
 		
+		if(score < 0 || score > 100) {
+			System.out.println("score error");
+			return; // 方法結束
+		}
+		
+		switch (score/10) {
+			case 10:
+			case 9:
+				System.out.println("A, Excellent!");
+				break;
+			case 8:
+				System.out.println("B, Well done");
+				break;				
+			case 7:
+				System.out.println("C, Well done");
+				break;
+			case 6:
+				System.out.println("D, You passed");
+				break;
+			default:
+				System.out.println("F, Better try again");
+				
+		}
 		
 
 	}
---
+++ javacode/day4/WhileDemo1.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class WhileDemo1 {
+
+	public static void main(String[] args) {
+		
+		double n = Math.random()*100; // 0~1
+		System.out.println(n);
+		System.out.println((int)n);
+
+	}
+
+}
