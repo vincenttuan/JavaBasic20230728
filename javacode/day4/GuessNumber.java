34f7ca13dc5a6653c25cce28925d083f3c0774de
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-07 21:26:50 +0800

    Add files via upload

--- javacode/day4/GuessNumber.java
+++ javacode/day4/GuessNumber.java
@@ -1,21 +1,24 @@
 package day4;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessNumber {
 
 	public static void main(String[] args) {
-		int ans = 21;
+		Random random = new Random();
 		int min = 0;
 		int max = 100;
+		//int ans = random.nextInt(99) + 1; // 1~99 之間的隨機數
+		int ans = random.nextInt(max-min-1) + 1;
 
 		while(true) {
 			Scanner sc = new Scanner(System.in);
-			System.out.printf("%d ~ %d 之間猜一數字:", min, max);
+			System.out.printf("User %d ~ %d 之間猜一數字:", min, max);
 			int userGuess = sc.nextInt();
 			// 判斷 user 所輸入的是否合法?
 			if(userGuess <= min || userGuess >= max) {
-				System.out.printf("您所輸入的數字 %d 不在 %d ~ %d 之間,請重新輸入\n", userGuess, min, max);
+				System.out.printf("User 所輸入的數字 %d 不在 %d ~ %d 之間,請重新輸入\n", userGuess, min, max);
 				continue;
 			}
 
@@ -25,7 +28,20 @@ public class GuessNumber {
 			} else if(userGuess < ans) {
 				min = userGuess;
 			} else {
-				System.out.println("恭喜答對了");
+				System.out.println("恭喜 User 答對了");
+				break;
+			}
+
+			// pc 猜 ---------------------------------------------------------------------------------------
+			int pcGuess = random.nextInt(max-min-1) + 1 + min;
+			System.out.printf("PC %d ~ %d 之間猜一數字:%d\n", min, max, pcGuess);
+			// 判斷 pc 是否猜對
+			if(pcGuess > ans) {
+				max = pcGuess;
+			} else if(pcGuess < ans) {
+				min = pcGuess;
+			} else {
+				System.out.println("恭喜 PC 答對了");
 				break;
 			}
 
