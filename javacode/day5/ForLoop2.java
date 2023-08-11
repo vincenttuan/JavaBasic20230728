b1eaf8c727381e8c5b91a74efbbb9b412b73d96d
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-11 21:26:04 +0800

    Add files via upload

---
+++ javacode/day5/ForLoop2.java
@@ -0,0 +1,17 @@
+package day5;
+
+/*
+請用 for loop 寫出 2024 ~ 2050 的閏年
+閏年的判斷規則如下：
+1. 如果一年能被4整除且不能被100整除，那麼這一年就是閏年。
+2. 但是，能被400整除的年份也是閏年。
+*/
+
+public class ForLoop2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
